e357b8d7606b8ea8209557c8ed265eeca1c71c9f
Nerdy Deeds, LLC <nerdy.deeds@done.cheap>
2018-10-11 15:56:36 -0500

    master

---
+++ public/lib/js/master.js
@@ -0,0 +1,8 @@
+// Site-wide color schema (// +SCSS equivalent variable name)
+const siteColor1 = '#FCC79D'; // $color1
+const siteColor2 = '#E7DED8'; // $color2
+const siteColor3 = '#DAD2CD'; // $color3
+const siteColor4 = '#C3BDBA'; // $color4
+const siteColor5 = '#696567'; // $color5
+const siteColor6 = '#383638'; // $color6
+const siteColor7 = '#FFFFFF'; // $color7
